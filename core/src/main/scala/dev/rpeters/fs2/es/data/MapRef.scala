62f9ec0772c7be9c78a9aa9404fcd755dbb9b91c
Ryan Peters <me@rpeters.dev>
2021-02-01 00:54:44 -0600

    Add MapRef ability to construct with separate effects

--- core/src/main/scala/dev/rpeters/fs2/es/data/MapRef.scala
+++ core/src/main/scala/dev/rpeters/fs2/es/data/MapRef.scala
@@ -23,29 +23,33 @@ sealed trait MapRef[F[_], K, V] {
 }
 
 object MapRef {
-  final class MapRefPartiallyApplied[F[_]: Sync]() {
-    private def mapFromRef[K, V](ref: Ref[F, Map[K, V]]) = new MapRef[F, K, V] {
-      def add(kv: (K, V)): F[Unit] = ref.update(_ + kv)
-      def del(k: K): F[Boolean] = ref.modify(m => m.get(k).fold(m -> false)(_ => m - k -> true))
-      def get(k: K): F[Option[V]] = ref.get.map(_.get(k))
-      def modify[A](k: K)(f: V => (V, A)): F[Option[A]] = ref.modify { map =>
+  final class MapRefPartiallyApplied[F[_]: Sync, G[_]: Sync]() {
+    private def mapFromRef[K, V](ref: Ref[G, Map[K, V]]) = new MapRef[G, K, V] {
+      def add(kv: (K, V)): G[Unit] = ref.update(_ + kv)
+      def del(k: K): G[Boolean] = ref.modify(m => m.get(k).fold(m -> false)(_ => m - k -> true))
+      def get(k: K): G[Option[V]] = ref.get.map(_.get(k))
+      def modify[A](k: K)(f: V => (V, A)): G[Option[A]] = ref.modify { map =>
         val resultOpt = map.get(k).map(f)
         resultOpt.fold(map -> Option.empty[A]) { result =>
           (map + (k -> result._1), result._2.some)
         }
       }
-      def upsertOpt[A](k: K)(f: Option[V] => (V, A)): F[A] = ref.modify { map =>
+      def upsertOpt[A](k: K)(f: Option[V] => (V, A)): G[A] = ref.modify { map =>
         val (v, a) = f(map.get(k))
         (map + (k -> v), a)
       }
     }
 
     /** Construct an empty `MapRef`. */
-    def empty[K, V] = Ref[F].of(Map.empty[K, V]).map(mapFromRef)
+    def empty[K, V] = Ref.in[F, G, Map[K, V]](Map.empty).map(mapFromRef)
 
     /** Construct a `MapRef` from a map of pure values. */
-    def of[K, V](map: Map[K, V]) = Ref[F].of(map).map(mapFromRef)
+    def of[K, V](map: Map[K, V]) = Ref.in[F, G, Map[K, V]](map).map(mapFromRef)
   }
 
-  def apply[F[_]: Sync] = new MapRefPartiallyApplied[F]
+  /** A set of constructors for `MapRef` using the same effect type for everything. */
+  def apply[F[_]: Sync] = new MapRefPartiallyApplied[F, F]
+
+  /** A set of constructors for `MapRef` where you can use a different effect for your internal `MapRef`. */
+  def in[F[_]: Sync, G[_]: Sync] = new MapRefPartiallyApplied[F, G]
 }
