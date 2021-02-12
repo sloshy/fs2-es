c2db8e1b0fd337f0657ed2b810e9f8868425f477
Ryan Peters <me@rpeters.dev>
2021-02-11 22:49:38 -0600

    Rewrite EventStateCache based on EventLog and typeclasses

--- core/src/main/scala/dev/rpeters/fs2/es/EventStateCache.scala
+++ core/src/main/scala/dev/rpeters/fs2/es/EventStateCache.scala
@@ -1,98 +1,97 @@
 package dev.rpeters.fs2.es
 
 import cats.implicits._
-import cats.effect._
-import fs2.{Pipe, Stream}
-import scala.concurrent.duration.FiniteDuration
+import cats.effect.{Concurrent, Sync, Timer}
 import cats.effect.concurrent.Ref
-
+import fs2.{Pipe, Stream}
 import data._
+import syntax._
+
+import scala.concurrent.duration._
 
 /** Caches EventState values by key, allowing you to use event-sourced state repeatedly. */
 sealed trait EventStateCache[F[_], K, E, A] {
 
-  /** Access some event state by key if it exists. */
-  def use[B](k: K)(f: EventState[F, E, A] => F[B]): F[Option[B]]
-
-  /** Add a new state to the manager by key. */
-  def add(k: K): F[Boolean]
-
-  /** Forwards events to their `EventState` by key.
-    * Emits key/new state pairs (`None` if state does not exist)
+  /** Access some state by key if it exists in your event log.
+    *
+    * If state is in-memory, it is immediately accessed and passed to your function.
+    * If state is in-memory but "deleted", immediately returns `None`.
+    * If state is not in-memory but exists in the event log, it is rebuilt from your event log and then passed to your function.
+    *
+    * @param k The key of your event-sourced state.
+    * @param f A function you want to apply to your entity state.
+    * @return `None` if the state was deleted or not found, `Some(b)` if your state exists and your function applied successfully.
     */
-  val hookup: Pipe[F, (K, E), (K, Option[A])]
+  def use[B](k: K)(f: A => F[B]): F[Option[B]]
 
-  /** Forwards events to a specific `EventState` by key. See `hookup`. */
-  def hookupKey(k: K): Pipe[F, E, (K, Option[A])] = _.map(e => k -> e).through(hookup)
+  /** Applies an event to your event log, and then to any in-memory states.
+    *
+    * @param e The event you are applying to your event log and state.
+    * @return `None` if the state was deleted or not found, `Some(a)` if your state exists or was created from this event.
+    */
+  def add(e: E): F[Option[A]]
+
+  /** Applies a stream of new events, first to the event log and then in-memory states.
+    *
+    * This is an `fs2.Pipe` which is an alias for a function `Stream[F, E] => Stream[F, Option[A]]`.
+    * By passing a stream of events to this `EventStateCache`,
+    * you will keep any in-memory states updated as well as get a stream of all resulting states.
+    */
+  val hookup: Pipe[F, E, Option[A]] = _.evalMap(e => add(e))
 }
 
 object EventStateCache {
 
   final class EventStateCachePartiallyApplied[F[_]: Sync, G[_]: Concurrent]() {
 
-    /** Rehydrates entities by key as-needed. */
-    def rehydrating[K, E, A](initializer: K => A)(keyHydrator: K => Stream[G, E])(eventProcessor: (E, A) => A)(
-        dur: FiniteDuration,
-        existenceCheck: K => G[Boolean] = (k: K) => keyHydrator(k).take(1).compile.last.map(_.isDefined)
-    )(implicit ev: Timer[G]) =
-      for {
-        deferredMap <- DeferredMap.in[F, G].tryableEmpty[K, Option[ExpiringRef[G, EventState[G, E, A]]]]
-      } yield new EventStateCache[G, K, E, A] {
-        def use[B](k: K)(f: EventState[G, E, A] => G[B]): G[Option[B]] = {
-          val getEph = deferredMap.getOrAddF(k) {
-            val hydrateStream = keyHydrator(k)
-            for {
-              wasHydratedRef <- Ref[G].of(false)
-              es <- EventState[G].initial[E, A](initializer(k))(eventProcessor)
-              _ <- es.hydrate(hydrateStream.evalTap(_ => wasHydratedRef.set(true)))
-              wasHydrated <- wasHydratedRef.get
-              result <-
-                if (wasHydrated) {
-                  //Entity does exist
+    /** Create an EventStateCache backed by the given `EventLog`.
+      * States that are used are kept in-memory for the specified duration between-uses, before they are removed.
+      *
+      * @param log Your event log you are restoring state from.
+      * @param dur The duration for states to remain in memory. Default is 2 minutes.
+      * @param existenceCheck A function to quickly determine if a state exists or not before consulting the event log.
+      * @return A new `EventStateCache` that loads states into memory and temporarily caches them.
+      */
+    def fromEventLog[K, E, A](
+        log: EventLog[G, E, E],
+        dur: FiniteDuration = 2.minutes,
+        existenceCheck: K => G[Boolean] = (_: K) => true.pure[G]
+    )(implicit driven: DrivenInitial[E, A], keyed: Keyed[K, E], timer: Timer[G]) = for {
+      deferredMap <- DeferredMap.in[F, G].tryableEmpty[K, Option[ExpiringRef[G, EventState[G, E, Option[A]]]]]
+    } yield new EventStateCache[G, K, E, A] {
+      private def useEventState[B](k: K)(f: EventState[G, E, Option[A]] => G[B]): G[Option[B]] = {
+        val doTheThing = deferredMap
+          .getOrAddF(k) {
+            log.getOneState[A, K](k).unNone.compile.last.flatMap {
+              case Some(state) =>
+                EventState[G].initial[E, A](state).flatMap { es =>
                   ExpiringRef[G]
                     .timed(es, dur)
-                    .flatTap { newEph =>
-                      Concurrent[G].start(newEph.expired >> deferredMap.del(k))
-                    }
+                    .flatTap(eph => Concurrent[G].start(eph.expired >> deferredMap.del(k)))
                     .map(_.some)
-                } else {
-                  //Entity does not exist
-                  Option.empty.pure[G]
                 }
-            } yield result
-          }
-          getEph.flatMap {
-            case Some(eph) => eph.use(f)
-            case None      => Option.empty.pure[G].flatTap(_ => deferredMap.del(k))
-          }
-        }
-        def add(k: K): G[Boolean] = {
-          existenceCheck(k).ifM(
-            {
-              false.pure[G]
-            }, {
-
-              deferredMap
-                .tryGet(k)
-                .flatMap {
-                  case Some(Some(_)) =>
-                    false.pure[G]
-                  case _ =>
-                    for {
-                      es <- EventState[G].initial[E, A](initializer(k))(eventProcessor)
-                      eph <- ExpiringRef[G].timed(es, dur)
-                      _ <- deferredMap.addPure(k)(eph.some)
-                      _ <- Concurrent[G].start(eph.expired >> deferredMap.del(k))
-                    } yield true
-                }
-
+              case None =>
+                none.pure[G]
             }
-          )
-        }
-        val hookup: Pipe[G, (K, E), (K, Option[A])] = { s =>
-          s.evalMap { case (k, e) => use(k)(es => es.doNext(e)).map(k -> _) }
-        }
+          }
+          .flatMap {
+            case Some(eph) => eph.use(f)
+            case None      => none[B].pure[G].flatTap(_ => deferredMap.del(k))
+          }
+
+        existenceCheck(k).ifM(doTheThing, none.pure[G].flatTap(_ => deferredMap.del(k)))
       }
+
+      def use[B](k: K)(f: A => G[B]): G[Option[B]] = {
+        useEventState(k)(_.get.flatMap(_.traverse(f))).map(_.flatten)
+      }
+
+      def add(e: E): G[Option[A]] = useEventState(e.getKey)(es => log.add(e) >> es.doNext(e)).flatMap {
+        case Some(s) => s.pure[G]
+        case None    => log.add(e) >> deferredMap.del(e.getKey).as(none)
+      }
+
+    }
   }
 
   /** A set of constructors for `EventStateCache` using the same effect type for everything. */
