a6c348fd49f52fa3cc6815e008f95085f793ae07
Ryan Peters <me@rpeters.dev>
2021-02-12 13:51:11 -0600

    Rebase BaseTestSpec on munit cats effect support

--- core/src/test/scala/dev/rpeters/fs2/es/BaseTestSpec.scala
+++ core/src/test/scala/dev/rpeters/fs2/es/BaseTestSpec.scala
@@ -4,8 +4,4 @@ import cats.effect.laws.util.TestContext
 import cats.effect.{ContextShift, IO, Timer}
 import munit.{CatsEffectSuite, ScalaCheckEffectSuite}
 
-abstract class BaseTestSpec extends CatsEffectSuite with ScalaCheckEffectSuite {
-  val tc = TestContext()
-  override implicit def munitContextShift: ContextShift[IO] = tc.contextShift[IO]
-  override implicit def munitTimer: Timer[IO] = tc.timer[IO]
-}
+abstract class BaseTestSpec extends CatsEffectSuite with ScalaCheckEffectSuite
