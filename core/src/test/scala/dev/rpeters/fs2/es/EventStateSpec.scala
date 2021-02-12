a97ba6aa9f46fcd69c327ea220b94a94771b1596
Ryan Peters <me@rpeters.dev>
2021-02-12 13:51:28 -0600

    Rewrite EventStateSpec

--- core/src/test/scala/dev/rpeters/fs2/es/EventStateSpec.scala
+++ core/src/test/scala/dev/rpeters/fs2/es/EventStateSpec.scala
@@ -3,35 +3,112 @@ package dev.rpeters.fs2.es
 import cats.effect._
 import cats.syntax.all._
 import fs2.Stream
-import org.scalacheck.Prop._
+import org.scalacheck.effect.PropF._
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
 
 import scala.concurrent.ExecutionContext.Implicits._
 
 class EventStateSpec extends BaseTestSpec {
+  implicit val driven = new Driven[String, String] {
+    def handleEvent(a: String)(e: String): Option[String] = (a + e).some
+    def handleEvent(optA: Option[String])(e: String): Option[String] = optA match {
+      case Some(value) => (value + e).some
+      case None        => e.some
+    }
+  }
 
   val s = "string"
   val strs = List("1", "2", "3")
 
-  test("Hydrate should have the starting value if the hydrated stream never emits") {
-    val emptyStream = Stream.empty
-    val state = EventState[IO].initial[String, String](s) { (_, str) =>
-      s"failed, expected '$str'"
-    }
-    state.flatTap(_.hydrate(emptyStream)).flatMap(_.get).unsafeToFuture().map(r => assertEquals(r, s))
+  def testWithEachEmpty[E, A](f: EventState[IO, E, Option[A]] => IO[Unit])(implicit driven: Driven[E, A]) = {
+    val esList = for {
+      es <- EventState[IO].empty[E, A]
+      signal <- EventState.signalling[IO].empty[E, A]
+      topic <- EventState.topic[IO].empty[E, A]
+    } yield List(es, signal, topic)
+
+    esList.flatMap(_.traverse(f))
   }
-  test("Hydrate should hydrate state with the given hydrator stream") {
-    val eventStream = Stream.emits(strs)
-    val state = EventState[IO].initial[String, String](s) { (e, str) =>
-      str + e
-    }
-    state.flatTap(_.hydrate(eventStream)).flatMap(_.get).unsafeToFuture().map { result =>
-      assertEquals(result, strs.foldLeft(s)(_ + _))
+
+  def testWithEachInitial[E, A](
+      a: A
+  )(f: EventState[IO, E, Option[A]] => IO[Unit])(implicit driven: DrivenNonEmpty[E, A]) = {
+    val esList = for {
+      es <- EventState[IO].initial[E, A](a)
+      signal <- EventState.signalling[IO].initial[E, A](a)
+      topic <- EventState.topic[IO].initial[E, A](a)
+    } yield List(es, signal, topic)
+
+    esList.flatMap(_.traverse(f))
+  }
+
+  def testWithEachInitialDefault[E, A](
+      a: A
+  )(f: EventState[IO, E, A] => IO[Unit])(implicit driven: DrivenNonEmpty[E, A]) = {
+    val esList = for {
+      es <- EventState[IO].initialDefault[E, A](a)
+      signal <- EventState.signalling[IO].initialDefault[E, A](a)
+      topic <- EventState.topic[IO].initialDefault[E, A](a)
+    } yield List(es, signal, topic)
+
+    esList.flatMap(_.traverse(f))
+  }
+
+  test("empty, initial, and initialDefault should always start with the default value") {
+    forAllF { s: String =>
+      val empty = testWithEachEmpty[String, String](es => es.get.assertEquals(None))
+      val initial = testWithEachInitial[String, String](s)(es => es.get.assertEquals(Some(s)))
+      val default = testWithEachInitialDefault[String, String](s)(es => es.get.assertEquals(s))
+
+      empty >> initial >> default.void
     }
   }
-  test("Initial should have the starting value") {
-    val state = EventState[IO].initial[Any, String](s) { (_, str) =>
-      List("failed, expected", s"'$str'").mkString(" ")
+
+  test("doNext, doNextStream, hookup, and hookupWithInput should apply events to state") {
+    forAllF(arbitrary[String], Gen.nonEmptyListOf(arbitrary[String])) { (s: String, events: List[String]) =>
+      val empty = testWithEachEmpty[String, String] { es =>
+        events.traverse(es.doNext).map(_.flattenOption) >> es.get.assertEquals(Some(events.mkString))
+      }
+      val emptyStream = testWithEachEmpty[String, String] { es =>
+        es.doNextStream(Stream.emits(events)) >> es.get.assertEquals(Some(events.mkString))
+      }
+      val emptyHookup = testWithEachEmpty[String, String] { es =>
+        es.hookup(Stream.emits(events)).compile.drain >> es.get.assertEquals(Some(events.mkString))
+      }
+      val emptyWithInput = testWithEachEmpty[String, String] { es =>
+        Stream.emits(events).through(es.hookupWithInput).compile.drain >> es.get.assertEquals(Some(events.mkString))
+      }
+      val initial = testWithEachInitial[String, String](s)(es =>
+        events.traverse(es.doNext) >> es.get.assertEquals(Some(s + events.mkString))
+      )
+      val initialStream = testWithEachInitial[String, String](s) { es =>
+        es.doNextStream(Stream.emits(events)) >> es.get.assertEquals(Some(s + events.mkString))
+      }
+      val initialHookup = testWithEachInitial[String, String](s) { es =>
+        es.hookup(Stream.emits(events)).compile.drain >> es.get.assertEquals(Some(s + events.mkString))
+      }
+      val initialWithInput = testWithEachInitial[String, String](s) { es =>
+        Stream.emits(events).through(es.hookupWithInput).compile.drain >> es.get.assertEquals(Some(s + events.mkString))
+      }
+      val default = testWithEachInitialDefault[String, String](s)(es =>
+        events.traverse(es.doNext) >> es.get.assertEquals(s + events.mkString)
+      )
+      val defaultStream = testWithEachInitialDefault[String, String](s) { es =>
+        es.doNextStream(Stream.emits(events)) >> es.get.assertEquals(s + events.mkString)
+      }
+      val defaultHookup = testWithEachInitialDefault[String, String](s) { es =>
+        es.hookup(Stream.emits(events)).compile.drain >> es.get.assertEquals(s + events.mkString)
+      }
+      val defaultWithInput = testWithEachInitialDefault[String, String](s) { es =>
+        Stream.emits(events).through(es.hookupWithInput).compile.drain >> es.get.assertEquals(s + events.mkString)
+      }
+
+      val testEmpty = empty >> emptyStream >> emptyHookup >> emptyWithInput
+      val testInitial = initial >> initialStream >> initialHookup >> initialWithInput
+      val testDefault = default >> defaultStream >> defaultHookup >> defaultWithInput
+
+      testEmpty >> testInitial >> testDefault.void
     }
-    state.flatMap(_.get).unsafeToFuture().map(r => assertEquals(r, s))
   }
 }
