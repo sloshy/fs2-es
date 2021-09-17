9f22434680f77857f973f3b7b977750b0e555a60
Scala Steward <me@scala-steward.org>
2021-09-17 19:53:32 +0200

    Reformat with scalafmt 3.0.4

--- core/src/main/scala/dev/rpeters/fs2/es/KeyedState.scala
+++ core/src/main/scala/dev/rpeters/fs2/es/KeyedState.scala
@@ -5,9 +5,12 @@ import syntax._
 
 /** Describes some state that can be initialized by a key, and driven by events.
   *
-  * @param K The key type you can extract from events.
-  * @param E The type of the events.
-  * @param A The type of state the events are applied to.
+  * @param K
+  *   The key type you can extract from events.
+  * @param E
+  *   The type of the events.
+  * @param A
+  *   The type of state the events are applied to.
   */
 trait KeyedState[K, E, A] extends Driven[E, A] with Keyed[K, E]
 
@@ -15,16 +18,18 @@ object KeyedState {
 
   def apply[K, E, A](implicit instance: KeyedState[K, E, A]) = instance
 
-  /** Define this type `A` as driven by events `E` with keys `K` in each event.
-    * Alias for `instance` but without implicits.
+  /** Define this type `A` as driven by events `E` with keys `K` in each event. Alias for `instance` but without
+    * implicits.
     *
-    * @return An instance of `KeyedState` for your state type.
+    * @return
+    *   An instance of `KeyedState` for your state type.
     */
   def from[K, E, A](driven: Driven[E, A], keyed: Keyed[K, E]) = instance(driven, keyed)
 
   /** Define this type `A` as driven by events `E` with keys `K` in each event.
     *
-    * @return An instance of `KeyedState` for your state type.
+    * @return
+    *   An instance of `KeyedState` for your state type.
     */
   def instance[K, E, A](implicit driven: Driven[E, A], keyed: Keyed[K, E]) =
     new KeyedState[K, E, A] {
